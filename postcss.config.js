b84d959a55e79462acc799b7987293f3f1e4aeb4
mm.oo.mm <goligo.lego.land@gmail.com>
2024-08-13 16:07:25 +0900

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {
+      flexbox: "no-2009",
+      grid: "autoplace",
+      overrideBrowserslist: ["last 2 versions", "not dead", "not op_mini all"]
+    }
+  }
+};
